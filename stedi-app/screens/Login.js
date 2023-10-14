d88f0d55a807999d33c5839a634684191b21ee28
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2023-10-14 00:33:38 -0500

    alert.alert (hello world)

--- stedi-app/screens/Login.js
+++ stedi-app/screens/Login.js
@@ -58,21 +58,23 @@ const Login = ({loggedInState, loggedInStates,setLoggedInState})=>{
               onPress={async ()=>{
                 console.log(phoneNumber+' Button was pressed')
     
-                const sendTextResponse=await fetch(
-                  'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
-                  {
-                    method:'POST',
-                    headers:{
-                     'content-type':'application/text'
-                   }
-                  }
-                )
-                const sendTextResponseData = await sendTextResponse.text();
-                if(sendTextResponse.status!=200){//invalid phone number, send them to the signup page
-                  await Alert.alert("Did you type your number correctly? "+phoneNumber);
-                } else{
-                  setLoggedInState(loggedInStates.LOGGING_IN);
-                }
+                Alert.alert("Hello Nathan!");
+
+                // const sendTextResponse=await fetch(
+                //   'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
+                //   {
+                //     method:'POST',
+                //     headers:{
+                //      'content-type':'application/text'
+                //    }
+                //   }
+                // )
+                // const sendTextResponseData = await sendTextResponse.text();
+                // if(sendTextResponse.status!=200){//invalid phone number, send them to the signup page
+                //   await Alert.alert("Did you type your number correctly? "+phoneNumber);
+                // } else{
+                //   setLoggedInState(loggedInStates.LOGGING_IN);
+                // }
               }}
             >
               <Text style={{color:'white'}}>Send</Text>      
