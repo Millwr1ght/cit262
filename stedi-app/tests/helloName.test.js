b498a2a20271e4409c67144d38ef935dd8bb9599
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2023-10-17 17:00:45 -0500

    Update helloName.test.js

--- stedi-app/tests/helloName.test.js
+++ stedi-app/tests/helloName.test.js
@@ -1,5 +1,11 @@
 import helloName from "../utils/helloName";
 
+//auto test
 it("outputs a name", () => {
     expect(helloName()).toBe("hello Nathan");
 })
+
+//for example
+//it("does the thing if", ()=>{
+//    expect(thingtotest()).toBe("result");
+//})
